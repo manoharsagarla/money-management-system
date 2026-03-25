a02c078e8bf0b6e7e2e4e7ad2c1a4c162eda0d54
manoharsagarla <sagarlamanu17@gmail.com>
2026-03-26 01:19:41 +0530

    fix mongo connection

--- backend/server.js
+++ backend/server.js
@@ -9,9 +9,12 @@ app.use(cors());
 app.use(express.json());
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log("MongoDB connected"))
-  .catch(err => console.log(err));
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => console.log("MongoDB connected"))
+.catch(err => console.log(err));
 // Test route
 app.get("/", (req, res) => {
   res.send("Backend Running");
